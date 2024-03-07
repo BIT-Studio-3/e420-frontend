3973c297635a4683de78073d076202e1fefbce48
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2024-03-08 11:33:35 +1300

    build(javascript, html): created contracts page, added some HTML containers and started a fetch function

---
+++ src/routes/(website)/contracts/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from "svelte"
+    const BASE_URL = ""
+    const key =""
+    let contracts = []
+
+    onMount(async () => {
+        
+    })
+    
+</script>
+
+<div class="page-container">
+    <div class="contracts-row">
+        <div class="contract-column">
+
+        </div>
+    </div>
+</div>
