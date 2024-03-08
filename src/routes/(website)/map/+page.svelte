097e7bab91dac3ba5f94d7cf047723fb1f779eec
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2024-03-08 13:26:56 +1300

    started th fetching for the location, need to write some code to get the symbol location now

--- src/routes/(website)/map/+page.svelte
+++ src/routes/(website)/map/+page.svelte
@@ -1,5 +1,29 @@
 <script>
 //This will be he java script code for the fetching of the location
+import { onMount } from 'svelte';
+    
+    let locationInfo = {};
 
+    const BearerToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiS1RHVFRJIiwidmVyc2lvbiI6InYyLjEuNSIsInJlc2V0X2RhdGUiOiIyMDI0LTAyLTI1IiwiaWF0IjoxNzA5ODU2OTY5LCJzdWIiOiJhZ2VudC10b2tlbiJ9.lCNa9BGVnVgx-_Sbpf9-g0PqUt1fsvxbFEXF3NzVmc4wXYntqXwp9h3bjovUY23ld7Hmyz_i76X-NVYxbJCJJG7QGX2YLDnD2QwMdYMNalB0Cm136TcHrv__6lztKxEOrLpqdRaIAyX5zvMzCKFAXCQTY2cYeJwJLPY_0S6zgxcmgAbTLkGIoQ4IK_bhkkoZ84DgAcwBn4pKQWI4USgO7Erua6oIZZEKnAgI54v9bOlbQIPXHDABlI4dN_hPFezlFdMD7vXXThL4bfj-cvcjtGlPHMYr_lBZROMQroKtRjkqwKZnwaa6c13lplQ9RONwr3TsM5hMy8mGNsNZmtrKvA'; // Replace 'YourBearerToken' with your actual token
+    const locationSymbol = 'YourLocationSymbol'; // Replace 'YourLocationSymbol' with the symbol of the location you want to get info for
+
+    onMount(async () => {
+        try {
+            const response = await fetch(`https://api.spacetraders.io/locations/${locationSymbol}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${BearerToken}`
+                }
+            });
+
+            if (response.ok) {
+                locationInfo = await response.json();
+            } else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    });
 </script>
 
