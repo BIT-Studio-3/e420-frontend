9261996cbccf47ec4c56e651cad1bce82fef55fe
Sophie Dickerson <sophieskyed@gmail.com>
2024-03-13 00:23:10 +1300

    was able to print the location info, had to change location to =  null and then use #if location

--- src/routes/(website)/map/+page.svelte
+++ src/routes/(website)/map/+page.svelte
@@ -2,20 +2,16 @@
 //This will be he java script code for the fetching of the location
 import { onMount } from 'svelte';
     
-let locationInfo = {
-        type: '',
-        symbol: '',
-        x: 0,
-        y: 0,
-        orbitals: [],
-        systemSymbol: '',
-        faction: {
-            symbol: ''
-        }
-    }; 
+ let locationInfo = null;
+//{
+//         type: '',
+//         symbol: '',
+//         x: 0,
+//         y: 0,
+//     }; 
 
     const BearerToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiS0lTU01FTU9SRSIsInZlcnNpb24iOiJ2Mi4yLjAiLCJyZXNldF9kYXRlIjoiMjAyNC0wMy0xMCIsImlhdCI6MTcxMDEyMDMwNSwic3ViIjoiYWdlbnQtdG9rZW4ifQ.h1pTisiPEzbnQ2WuYRunlvA_pj7AbposfZALOKEIgJ15u_F-J3v3woGCsHMXEFOpnViFS6cJMQ5LfEan0cY8teybxXZq7yHGUKDp4ILvpbEB8ccB1Au8cv3IbLjnHt6-P5iwNqOg5IiHiSK1dFZ68JxYa8rl63pSWxuwPejigmt_ywIIrQNtOUaijpkICqapvEEgFr4RM_wAdN7lDR3ThhPoTM15dPLbv9VmYrIZHgKWuwwqvqtdK4Ng98-EcjZsYYWLvJF_ygUH7CZTL7-1xgUMJ-jCq9Val8WesS4T1t4nLyQtaW6UgCB0IxlS3jcXFdaRzWx1pXCCnI5dqz5paA';
-    const locationSymbol = 'X1-NT89-A1'; // OE is the symbol for Earth 
+    const locationSymbol = 'X1-NT89-A1'; // headquaters symbol
     
     onMount(async () => {
         try {
@@ -27,32 +23,34 @@ let locationInfo = {
                 }
             });
 
-                console.log('Location information received successfully.');
-                locationInfo = await response.json();
+            const responseData = await response.json();
+            locationInfo = responseData; // Assign fetched data to the 'data' variable
+            console.log('Location information received successfully:');
 
         } catch (error) {
             console.error('Error:', error);
         }
-        console.log(locationInfo)
+         console.log(locationInfo)
     });
-    console.log(locationInfo)
+    // console.log(locationInfo)
 </script>
 
 <body>
     <div id="location-info">
-        <h1>Loading...</h1>
-        <p>Fetching location information...</p>
+      <h1>Loading...</h1>
+      <p>Fetching location information...</p>
+      {#if locationInfo} 
         <div id="location-details">
-            <p>Type: {locationInfo.type}</p>
-            <p>Symbol: {locationInfo.symbol}</p>
-            <p>X: {locationInfo.x}</p>
-            <p>Y: {locationInfo.y}</p>
-            <p>System Symbol: {locationInfo.systemSymbol}</p>
-            <p>Faction Symbol: {locationInfo.faction.symbol}</p>
-            <p>Orbitals: {locationInfo.orbitals.map(orbital => orbital.symbol).join(', ')}</p>
-        </div> <!-- Placeholder for location details -->
-    </div>
-</body>
+          <p>Type: {locationInfo.data.type}</p>
+          <p>Symbol: {locationInfo.data.symbol}</p>
+          <p>X: {locationInfo.data.x}</p>
+          <p>Y: {locationInfo.data.y}</p>
+          <!-- <p>System Symbol: {locationInfo.systemSymbol}</p>
+          <p>Orbitals: {locationInfo.orbitals.map(orbital => orbital.symbol).join(', ')}</p> -->
+        </div>
+      {/if}
+    </div>  
+  </body>
 
 
 <style>
