29f6714cf18d91c4c5a9cd3bf74377682046e510
TylorJB <tylorbennett18@gmail.com>
2024-03-18 16:40:54 +1300

    commented out unused css

--- src/routes/(signin)/login/+page.svelte
+++ src/routes/(signin)/login/+page.svelte
@@ -1,22 +1,16 @@
 <script>
-  
-
   function nameFunction() {
     // Redirecting to a different page when the button is clicked
     window.location.href = "/"; 
   }
-
-  
 </script>
 
 <button on:click={nameFunction}>Log In</button>
 
-
 <style>
-  label {
+  /* label {
     color: white;
   }
-
   input[type="text"] {
     width: 100%;
     padding: 12px 20px;
@@ -24,15 +18,13 @@
     display: inline-block;
     border: 1px solid #ccc;
     box-sizing: border-box;
-  }
-
+  } */
   button {
     background-color: white;
     color: black;
     padding: 14px 20px;
     margin: 8px 0;
     border: none;
-
     /*displays a pointer cursor only when hovering over the 
   button helps to show the user that they are in fact on the interactive button*/
     cursor: pointer;
