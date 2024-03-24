441c4108feb6757e6472980729a6baeabab5b2e2
mintep1-student <mintep1@student.op.ac.nz>
2024-03-24 19:02:18 +1300

    removed map page code and implemented shipyard data. Added html checks

--- src/routes/(website)/shipyard/+page.svelte
+++ src/routes/(website)/shipyard/+page.svelte
@@ -15,19 +15,21 @@
 
   onMount(async () => {
     try {
-      // location data fetch
+      // fetch shipyard from current waypoint
       const res = await fetch(
         "https://api.spacetraders.io/v2/systems/X1-NT89/waypoints/X1-NT89-A1/shipyard",
         options,
       );
 
       const json = await res.json();
-      shipyards = json.data; // Assign fetched data to the 'data' variable
+      shipyards = json.data;
+      // store error message if there's no shipyard at current location
       if (shipyards.error.code === 404) {
         shipyards = {
           msg: "No shipyard available at current waypoint",
         };
       }
+
       if (shipyards.transactions) {
         delete shipyards.transactions;
       }
@@ -40,20 +42,19 @@
 </script>
 
 <div id="shipyard-container">
-  <!-- this needs to be replaced with an actual loading animation -->
-  <h1>Loading...</h1>
-  <p>Fetching location information...</p>
-
-  <!-- The {#if locationInfo} directive is a conditional rendering block (in Svelte) that checks 
-  if the locationInfo variable is truthy (i.e., not null, undefined, or an empty string). 
-  If the condition is true, Svelte will render the content inside the block. 
-  If the condition is false, Svelte will skip over the block and not render its contents-->
-  {#if shipyards}
-    <div id="location-details">
-      <p>Type: {locationInfo.data.type}</p>
-      <p>Symbol: {locationInfo.data.symbol}</p>
-      <p>X: {locationInfo.data.x}</p>
-      <p>Y: {locationInfo.data.y}</p>
+<!-- add loading animation -->
+  {#if shipyards.symbol}
+  <div class="shipyard-box">
+    <div id="shipyard-details">
+      <p>Type: {shipyards.}</p>
+      <p>Symbol: {shipyards.}</p>
+      <p>X: {shipyards.}</p>
+      <p>Y: {shipyards.}</p>
+    </div>
+    </div>
+  {:else}
+    <div id="error">
+      <h1>{shipyards.msg}</h1>
     </div>
   {/if}
 </div>
