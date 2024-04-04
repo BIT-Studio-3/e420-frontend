503bc37dbf4fd25702d20a87da568d54162de38a
TylorJB <tylorbennett18@gmail.com>
2024-04-05 11:28:21 +1300

    created a css page, this contains variables that can be imported by other pages

---
+++ static/css/+page.svelte
@@ -0,0 +1,28 @@
+<style>
+    /* all of these variables can be used by:
+    - @import url('static/css/+page.svelte'); */
+
+    /* The variables can then be used by including this in your styling:
+    body {
+    color: var(--color);
+    font-family: var(--font);
+    } */
+
+.box {
+  border-radius: var(--border-radius);
+}
+:root {
+    --color: #3BBA9C;
+    --font: 'Orbitron', sans-serif;
+    --border-radius: 25px;
+}
+
+body {
+    color: var(--color);
+    font-family: var(--font);
+}
+
+.box {
+  border-radius: var(--border-radius);
+}
+</style> 
