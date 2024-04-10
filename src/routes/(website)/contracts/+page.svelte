1d988f849e046856d1c6c7aa7731a17862786e0b
St1ckyf0x <BIGGMS1@student.op.ac.nz>
2024-04-10 14:19:54 +1200

    previous commit - token I was testing with

--- src/routes/(website)/contracts/+page.svelte
+++ src/routes/(website)/contracts/+page.svelte
@@ -5,7 +5,7 @@
   let contractArr = null;
   let temp = [];
 
-  const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiVVVJVSIsInZlcnNpb24iOiJ2Mi4yLjAiLCJyZXNldF9kYXRlIjoiMjAyNC0wMy0yNCIsImlhdCI6MTcxMTQyMDE0MSwic3ViIjoiYWdlbnQtdG9rZW4ifQ.YDTD_INb92K9ouz-9o-H_W072RrNUOOx67hvd3yVUBn_GQCvYP5bNRgQNELBoxOmo4hTJ545W9i0LVlgdIKiu_WtYa4GJh9Jbpx8s6hbU7hZ2VBymM-zJvmQAjZjsrC2SBOpt98uV4L3vjeCxpb_1O0gS5xEqkGqiVAyw6J58HVIBCMhSP_nltV5-pJ6OWvXHBJ-YgqjvaEf26sjgMpgNliedAnpcxAtdu3ByGCslOwc-Zd0HU_hENBf0JfvZzl8vjuYgvDrjDI_4OsILM4sgUfbfzSEkRJtSA5va6VHRTLDxyaxeIdZLkrzZhcbWB_05PK-f9bcHCkM0ntZeMIZVw";
+  const token = "";
 
   const options = {
     method: "GET",
@@ -141,9 +141,9 @@
 
   .contract-box {
     width: 600px;
-    background-color: #2E3047; /* Change the background color of the box */
+    background-color: #2E3047; /* Changes the background color of the box */
     border-radius: 8px;
-    border: 1px solid #3BBA9C; /* Add a border with a color */
+    border: 1px solid #3BBA9C; /* Adds a border with a color */
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     text-align: center;
     margin: 0px 40px;
