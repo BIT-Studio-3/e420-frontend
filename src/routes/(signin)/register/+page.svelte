d2f640c07015e2754628b7389f2bf29aee1007a4
TylorJB <tylorbennett18@gmail.com>
2024-06-12 16:04:30 +1200

    Ive improved the body/page contents of the register page. It is currently a rough design and layout without much styling. Ive commented out code that is not needed currently and removed the code relating to the create button

--- src/routes/(signin)/register/+page.svelte
+++ src/routes/(signin)/register/+page.svelte
@@ -1,13 +1,33 @@
 <script>
-  function nameFunction() {
-    // Redirecting to a different page when the button is clicked
-    window.location.href = "/";
-  }
+  // function nameFunction() {
+  //   Redirecting to a different page when the button is clicked
+  //   window.location.href = "/";    
+  // }
+  
 </script>
 
-<label for="uname"><b>Create New User</b></label>
+<!-- <label for="uname"><b>Create New User</b></label>
 <input type="text" placeholder="Enter Username" name="uname" required />
-<button on:click={nameFunction}>Create</button>
+<button on:click={nameFunction}>Create</button> -->
+
+<div class="outer-box">
+  <label for="login-header"><b>Create New User</b></label>
+  <div class="content">
+    <hr class="line" />
+    <div class="input-container">
+      <input
+        placeholder="Enter your username"
+        class="input-username"
+        type="text"
+        id="username-input"
+      />
+    </div>
+    <div class="Create">
+      <button class="create-button" >Create</button>
+      <a href="/login" class="login-link">Click Here To login</a>
+    </div>
+  </div>
+</div>
 
 <!--When you click the button it activates nameFunction bellow-->
 
